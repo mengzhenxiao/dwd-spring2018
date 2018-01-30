fc7fa53c607045f12bd9f51ac81f6d756d147c6a
itpstudent <itpstudent@CLASS402.ITP.TSOA.NYU.EDU>
2018-01-30 14:07:00 -0500

    added static files

--- week2/server.js
+++ week2/server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
 app.get('/somethingelse', function(req, res) {
   res.send("Goodbye");
-  
+
 })
 
 app.listen(3000, function () {
