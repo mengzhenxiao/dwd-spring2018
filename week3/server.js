a8b90802c823b358daebb995814ec2567ca9c62b
Shawn Van Every <vanevery@walking-productions.com>
2018-02-06 14:27:04 -0500

    Using mongo

--- week3/server.js
+++ week3/server.js
@@ -1,5 +1,16 @@
+
+var config = require('./config.js');
+console.log(config);
+
+var mongojs = require('mongojs');
 var express = require('express');
 var app = express();
+
+var db = mongojs("dwd-spring2018:password@ds125588.mlab.com:25588/dwd-spring2018", ["thesubmissions"]);
+// db.thesubmissions.save({"test":"a test"}, function(err, saved) {
+//   if( err || !saved ) console.log("Not saved");
+//     else console.log("Saved");
+// });
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -16,12 +27,38 @@ var thesubmissions = [];
 
 app.get('/formpost', function(req, res) {
   //res.send("You submitted " + req.query.textfield);
-  thesubmissions.push(req.query.textfield);
+  // thesubmissions.push(req.query.textfield);
+
+  db.thesubmissions.save({"submission":req.query.textfield}, function(err, saved) {
+    if( err || !saved ) console.log("Not saved");
+      else console.log("Saved");
+  });
+
   res.redirect('/display');
 });
 
 app.get('/display', function(req, res) {
-  res.render('display.ejs', {thedata:thesubmissions});
+
+  db.thesubmissions.find({}, function(err, saved) {
+    if (err || !saved) {
+    	console.log("No results");
+    }
+    else {
+      console.log(saved);
+      res.render('display.ejs', {thedata:saved});
+
+    	// saved.forEach(function(record) {
+      // 	console.log(record);
+    	// });
+
+    	/* Alternatively you could loop through the records with a "for"
+    	for (var i = 0; i < saved.length; i++) {
+  	  	console.log(saved[i]);
+  	}
+  	*/
+    }
+  });
+
 });
 
 app.get('/count', function(req, res) {
